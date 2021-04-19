8b8fe70177bbf686aff5ada4fbd44e3e512a3362
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2021-04-19 20:11:52 +0400

    Version 3 (Little tweaks)

--- api/controllers/TestController.php
+++ api/controllers/TestController.php
@@ -4,9 +4,14 @@
 namespace app\api\controllers;
 
 use app\core\Http\Request;
+use app\core\Http\Response;
 
 class TestController {
-    public function log(Request $request) {
-        echo $request->method();
+    public function log(Request $request, Response $response) {
+        $method = $request->method();
+
+        $response->json([
+            'msg' => $method
+        ]);
     }
 }
